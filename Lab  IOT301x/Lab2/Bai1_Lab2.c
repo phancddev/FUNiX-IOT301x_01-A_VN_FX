051d08028c80ce95192f1e007202948145967bfe
phancddev <phancddev@gmail.com>
2022-07-22 12:25:45 +0700

    Add Lab 2 -p1

---
+++ Lab  IOT301x/Lab2/Bai1_Lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ /*
+    - This is lab 2.1, created by phancd.dev
+    - Date of creation: 21/07/2022
+    - Date of modified: 21/07/2022
+    - Goal of this project: Input and output data using scanf and printf
+    */
+    printf("Name: Phan Cong Dung \n");
+    printf("DOB: October 23, 2009 \n");
+    printf("Mobile:0916.127.062 \n");
+    return 0;
+
+}
