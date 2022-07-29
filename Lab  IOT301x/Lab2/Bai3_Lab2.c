e6ad4ecfdcdbbb048c5fb5a74dd83bd2e0937d4a
phancddev <phancddev@gmail.com>
2022-07-29 17:36:59 +0700

    Add lab2 -p5 - b3

--- Lab  IOT301x/Lab2/Bai3_Lab2.c
+++ Lab  IOT301x/Lab2/Bai3_Lab2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main() {
 /*
-    - This is lab 2.1, created by phancd.dev
+    - This is lab 2.3, created by phancd.dev
     - Date of creation: 21/07/2022
     - Date of modified: 21/07/2022
     - Goal of this project: Input and output data using scanf and printf
     */
+char ten[32];
+printf("What is your name:");
+scanf("%s", &ten);
+printf(" Hello %s. How are you? \n", ten);
 }
