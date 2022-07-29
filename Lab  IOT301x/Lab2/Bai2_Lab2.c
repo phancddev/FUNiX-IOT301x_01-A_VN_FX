035c50cfb7714a75b056995cbaeba76fd91dbb47
phancddev <phancddev@gmail.com>
2022-07-29 17:28:33 +0700

    Add -p4

--- Lab  IOT301x/Lab2/Bai2_Lab2.c
+++ Lab  IOT301x/Lab2/Bai2_Lab2.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
+/*
+    - This is lab 2.1, created by phancd.dev
+    - Date of creation: 21/07/2022
+    - Date of modified: 21/07/2022
+    - Goal of this project: Input and output data using scanf and printf
+    */
 
+   char d;
+   scanf("%c", &d);
+   printf("%c \n", d);
 return 0;
 }
---
+++ Lab  IOT301x/Lab2/Bai3_Lab2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+/*
+    - This is lab 2.1, created by phancd.dev
+    - Date of creation: 21/07/2022
+    - Date of modified: 21/07/2022
+    - Goal of this project: Input and output data using scanf and printf
+    */
+}
