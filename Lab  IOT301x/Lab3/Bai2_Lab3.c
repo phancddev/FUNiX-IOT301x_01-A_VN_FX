133c914e6a7bd4ca8c227d94fec0de07f1997fa4
phancddev <phancddev@gmail.com>
2022-08-03 10:50:35 +0700

    Ađ Lab3 -p3

---
+++ Lab  IOT301x/Lab3/Bai2_Lab3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+enum Company {Google, Facebook, Xerox, Yahoo, Ebay, Microsoft};
+enum Company thisCompany;
+thisCompany = Microsoft;
+printf("%d \n", thisCompany);
+
+}
