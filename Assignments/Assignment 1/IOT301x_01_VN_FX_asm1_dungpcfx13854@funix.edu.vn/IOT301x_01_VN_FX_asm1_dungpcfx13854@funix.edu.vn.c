c86085263f7df30631a15ac66ff5962ff5336eb8
phancddev <phancddev@gmail.com>
2022-08-24 01:10:11 +0700

    asm1 -1

---
+++ Assignments/Assignment 1/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#define htd "Xin vui long nhap toa do diem"
+float toaDo[6];
+float canhGoc[6];
+float duongCao[3];
+float trungTuyen[3];
+float chuVi;
+
+void duongCaoTamGiac() {
+    //Tinh nua chu vi
+    
+}
+
+int main()
+{
+    return 0;
+}
