b8d5a2e4e6de6553956c1572e906ba5f80f3b054
phancddev <phancddev@gmail.com>
2022-09-04 13:39:36 +0700

    clean - fix 2

--- Assignments/Assignment 1/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn.c
+++ Assignments/Assignment 1/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn/IOT301x_01_VN_FX_asm1_dungpcfx13854@funix.edu.vn.c
@@ -18,46 +18,30 @@ float p; //Chu vi
 void duongcao_tamgiac() {
     //Tinh nua chu vi
     p = (canhGoc[0] + canhGoc[1] + canhGoc[2]) / 2; 
-
-
     // Ap dung cong thuc heron, tinh do dai cua duong cao tam giac (duongCao)
     duongCao[0] = ((2 * (sqrt(p * (p - canhGoc[2]) * (p - canhGoc[1]) * (p - canhGoc[0])))) / canhGoc[2]);
     duongCao[1] = ((2 * (sqrt(p * (p - canhGoc[2]) * (p - canhGoc[1]) * (p - canhGoc[0])))) / canhGoc[1]);
     duongCao[2] = ((2 * (sqrt(p * (p - canhGoc[2]) * (p - canhGoc[1]) * (p - canhGoc[0])))) / canhGoc[0]);
-
     //Print 
-   
     printf(cach "3. So do nang cao tam giac ABC: \n");
     printf("Do dai duong cao tu dinh A la: %.2f \n", duongCao[0]); //%.2f có nghĩa là lấy 2 số lẻ sau dấu”.” ví dụ: 1.65 hay 1.74
     printf("Do dai duong cao tu dinh B la: %.2f \n", duongCao[1]); 
     printf("Do dai duong cao tu dinh C la: %.2f \n", duongCao[2]); 
-    
     }
-
-
 // Ham tinh dien tich tam giac
 void dientich_tamgiac() {
     p = (canhGoc[2] + canhGoc[1] + canhGoc[0]) / 2;
-
     //Ap dung cong thuc Heron, tinh dien tich
-
     float s = sqrt(p * (p - canhGoc[2]) * (p - canhGoc[1]) * (p - canhGoc[0]));
     printf(cach);
     printf("2. Dien tich tam giac la: %.2f \n", s);
- 
 }
 
-
 void tinhcanh() {
-
 // Tinh canh tu toa do da nhap vao
-
-canhGoc[0] = sqrt((toaDo[0] - toaDo[2]) * (toaDo[0] - toaDo[2]) + (toaDo[1] - toaDo[3]) * (toaDo[1] - toaDo[3]));
-canhGoc[1] = sqrt((toaDo[0] - toaDo[4]) * (toaDo[0] - toaDo[4]) + (toaDo[1] - toaDo[5]) * (toaDo[1] - toaDo[5]));
-canhGoc[2] = sqrt((toaDo[2] - toaDo[4]) * (toaDo[2] - toaDo[4]) + (toaDo[3] - toaDo[5]) * (toaDo[3] - toaDo[5]));
-
-
-
+    canhGoc[1] = sqrt((toaDo[0] - toaDo[4]) * (toaDo[0] - toaDo[4]) + (toaDo[1] - toaDo[5]) * (toaDo[1] - toaDo[5]));
+    canhGoc[2] = sqrt((toaDo[2] - toaDo[4]) * (toaDo[2] - toaDo[4]) + (toaDo[3] - toaDo[5]) * (toaDo[3] - toaDo[5]));
+    canhGoc[0] = sqrt((toaDo[0] - toaDo[2]) * (toaDo[0] - toaDo[2]) + (toaDo[1] - toaDo[3]) * (toaDo[1] - toaDo[3]));
 }
 
 void xet_tamgiac()
@@ -97,20 +81,21 @@ void xet_tamgiac()
     dacTinh[3] = '0';
 
   if (dacTinh[0] != '0' && dacTinh[2] != '0')
-    printf("Tam giac vuong can tai %c\n", dacTinh[0]);
+    printf("\nTam giac ABC la tam giac vuong can tai %c\n", dacTinh[0]);
   else if (dacTinh[1] != '0' && dacTinh[2] != '0')
-    printf("Tam giac tu va can tai %c", dacTinh[1]);
+    printf("\nTam giac ABC la tam giac tu va can tai %c", dacTinh[1]);
   else if (dacTinh[0] != '0')
-    printf("Tam giac vuong tai %c\n", dacTinh[0]);
+    printf("\nTam giac ABC la tam giac vuong tai %c\n", dacTinh[0]);
   else if (dacTinh[1] != '0')
-    printf("Tam giac tu tai %c\n", dacTinh[1]);
+    printf("\nTam giac ABC la tam giac tu tai %c\n", dacTinh[1]);
   else if (dacTinh[2] != '0')
-    printf("Tam giac can tai %c\n", dacTinh[2]);
+    printf("\nTam giac ABC la tam giac can tai %c\n", dacTinh[2]);
   else if (dacTinh[3] != '0')
-    printf("Tam giac deu\n");
+    printf("\nTam giac ABC la tam giac deu\n");
   else
-    printf("Tam giac nhon\n");
+    printf("\nTam giac ABC la tam giac nhon\n");
 }
+
 void tinhgoc() {
       // Tinh goc tu canh
   float cosA = (canhGoc[1] * canhGoc[1] + canhGoc[0] * canhGoc[0] - canhGoc[2] * canhGoc[2]) / (2 * canhGoc[0] * canhGoc[1]);
@@ -119,8 +104,8 @@ void tinhgoc() {
   canhGoc[3] = acos(cosA) * 180 / PI;
   canhGoc[4] = acos(cosB) * 180 / PI;
   canhGoc[5] = acos(cosC) * 180 / PI;
-
 }
+
 void trungtuyen_tamgiac()
 {
   // Tinh do dai trung tuyen
@@ -131,52 +116,44 @@ void trungtuyen_tamgiac()
   printf("Do dai trung tuyen tu dinh B la: %.2f \n", trungTuyen[1]);
   printf("Do dai trung tuyen tu dinh C la: %.2f \n", trungTuyen[2]);
 }
+
 void tam_tamgiac()
 {
   // Tinh toa do trong tam
   float g = (toaDo[0] + toaDo[2] + toaDo[4]) / 3;
   float h = (toaDo[1] + toaDo[3] + toaDo[5]) / 3;
   printf(cach);
-  printf( "4. Toa do diem dac biet cua tam giac ABC:\n");
+  printf("4. Toa do diem dac biet cua tam giac ABC:\n");
   printf("Toa do trong tam: [%.2f, %.2f]", g, h);
 }
 
-
-
-
 void goccanh_tamgiac() {
-
   //Goi ham tinh canh
 tinhcanh();
-
 // In canh
   printf(cach);
    //In canh
-  
-  printf("1. Cac so do co ban cua tam giac:\n");
-  printf("Chieu dai cua canh AB la: %.2f \n", canhGoc[0]);
-  printf("Chieu dai cua canh BC la: %.2f \n", canhGoc[2]);
-  printf("Chieu dai cua canh AC la: %.2f \n", canhGoc[1]);
-
+  printf("1. ");
+  printf("Cac so do co ban cua tam giac:\n");
+  printf("   Chieu dai canh AB la: %.2f \n", canhGoc[0]);
+  printf("   Chieu dai canh BC la: %.2f \n", canhGoc[2]);
+  printf("   Chieu dai canh CA la: %.2f \n", canhGoc[1]);
 //Goi ham tinhgoc
 tinhgoc();
-
 //In goc
-  printf("So do goc B la: %.2f \n", canhGoc[4]);
-  printf("So do goc C la: %.2f \n", canhGoc[5]);
-  printf("So do goc A la: %.2f \n", canhGoc[3]);
-  
+  printf("   Goc A: %.2f \n", canhGoc[4]);
+  printf("   Goc B: %.2f \n", canhGoc[5]);
+  printf("   Goc C: %.2f \n", canhGoc[3]);
 }
-
 
 void giaima_tamgiac()
 {
-goccanh_tamgiac();
-xet_tamgiac();
-dientich_tamgiac();
-duongcao_tamgiac();
-trungtuyen_tamgiac();
-tam_tamgiac();
+  goccanh_tamgiac();
+  xet_tamgiac();
+  dientich_tamgiac();
+  duongcao_tamgiac();
+  trungtuyen_tamgiac();
+  tam_tamgiac();
 }
 int check_tamgiac()
 {
